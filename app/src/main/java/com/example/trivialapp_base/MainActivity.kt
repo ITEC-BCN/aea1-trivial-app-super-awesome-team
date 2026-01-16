572138460de3184198f1c1518b608143f22666fd
AlexLopez75 <alex.lopez.7e9@itb.cat>
2026-01-16 10:49:48 +0100

    feat: added path finding (incomplete)

--- app/src/main/java/com/example/trivialapp_base/MainActivity.kt
+++ app/src/main/java/com/example/trivialapp_base/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,7 +12,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.trivialapp_base.ui.theme.TrivialAPP_BaseTheme
+import com.example.trivialapp_base.view.GameScreen
+import com.example.trivialapp_base.view.MenuScreen
+import com.example.trivialapp_base.view.ResultScreen
+import com.example.trivialapp_base.viewmodel.GameViewModel
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,18 +28,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TrivialAPP_BaseTheme {
-                // Controlador de navegación
-
-
-
-                // Instanciamos el ViewModel una vez
-
-
-
-                // Definición de rutas y navegación
-
-
-
+                val navigationController = rememberNavController()
+                val gameViewModel: GameViewModel by viewModels()
+                NavHost(
+                    navController = navigationController,
+                    startDestination = Routes.MenuScreen.route
+                ) {
+                    composable(Routes.MenuScreen.route) { MenuScreen(navigationController, gameViewModel) }
+                    composable(Routes.GameScreen.route) { GameScreen(navigationController, gameViewModel) }
+                    composable(Routes.ResultScreen.route) { ResultScreen(navigationController, gameViewModel) }
+                }
             }
         }
     }
--- app/src/main/java/com/example/trivialapp_base/view/Screen_menu.kt
+++ app/src/main/java/com/example/trivialapp_base/view/Screen_menu.kt
@@ -1,10 +1,60 @@
 package com.example.trivialapp_base.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.NavController
 import com.example.trivialapp_base.viewmodel.GameViewModel
+import androidx.compose.*
+import androidx.compose.ui.Alignment
+import com.example.trivialapp_base.R
+import com.example.trivialapp_base.Routes
 
 @Composable
 fun MenuScreen(navController: NavController, viewModel: GameViewModel) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+            .background(Color.White)
+    ) {
+        //val (logoRef, contentRef, btnRef) = createRefs()
 
+        Text(
+            text = "Trivial Pursuit Game",
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Image(
+            painter = painterResource(R.drawable.trivial_icon),
+            contentDescription = "Logo",
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .size(150.dp)
+        )
+
+        Button(
+            onClick = { navController.navigate(Routes.GameScreen.route) },
+            modifier = Modifier
+                .align(Alignment.Center as Alignment.Horizontal)
+        ) {
+            Text(text = "Start Game")
+        }
+    }
 }
