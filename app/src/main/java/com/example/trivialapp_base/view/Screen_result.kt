0e3f65a75dd5db09ed06cd58fca883960dc2bbb1
GFM204 <guzman.fernandez.melenchon@gmail.com>
2026-01-16 10:11:30 +0100

    Funtional result screen

--- app/src/main/java/com/example/trivialapp_base/view/Screen_result.kt
+++ app/src/main/java/com/example/trivialapp_base/view/Screen_result.kt
@@ -1,10 +1,32 @@
 package com.example.trivialapp_base.view
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import com.example.trivialapp_base.viewmodel.GameViewModel
 
 @Composable
 fun ResultScreen(navController: NavController, viewModel: GameViewModel) {
+    Box(
+        modifier = Modifier.
+        fillMaxSize(),
+        contentAlignment =  Alignment.Center){
+        Column() {
+            Text(text = "Your Score")
+            Text(text = "${viewModel.puntuacion}")
+            Button(onClick = {}) {
+                Text(text = "Share")
+            }
+            Button(onClick = {}) {
+                Text(text = "Return to menu")
+            }
+        }
 
+    }
 }
