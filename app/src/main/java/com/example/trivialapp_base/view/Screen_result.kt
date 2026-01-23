1ec2bb90365745323e4c296db7275c57c11b68bf
GFM204 <guzman.fernandez.melenchon@gmail.com>
2026-01-23 10:29:12 +0100

    feat: different image depending on the final result

--- app/src/main/java/com/example/trivialapp_base/view/Screen_result.kt
+++ app/src/main/java/com/example/trivialapp_base/view/Screen_result.kt
@@ -1,6 +1,7 @@
 package com.example.trivialapp_base.view
 
 import androidx.compose.*
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -18,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -41,38 +43,54 @@ fun ResultScreen(navController: NavController, viewModel: GameViewModel) {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
+            Image(
+                painter = if(viewModel.puntuacion <5) {
+                    painterResource(R.drawable.homer_simpson)
+                }else if(viewModel.puntuacion in 5..8){
+                    painterResource(R.drawable.promedio)
+                }else{
+                    painterResource(R.drawable.cerebro_galaxia)
+                },
+                contentDescription = "Imatge de resultat"
+
+            )
             Text(
                 modifier = Modifier
                     .padding(bottom = 21.dp),
                 text = "Your Score",
                 fontSize = 30.sp,
                 fontWeight = FontWeight.Bold)
+
             Text(
                 modifier = Modifier
                     .padding(horizontal = 30.dp),
                 text = "${viewModel.puntuacion}",
                 fontSize = 30.sp)
+
             Spacer(modifier = Modifier.height(16.dp))
+
             Button(onClick = {},
                 colors = ButtonDefaults.buttonColors(
                     containerColor = colorResource(R.color.yellow_custom),
                     contentColor = Color.Black),) {
-                Text(
-                    text = "Share",
-                    fontSize = 30.sp,
+                    Text(
+                        text = "Share",
+                        fontSize = 30.sp,
                     )
-            }
+                    }
+
             Spacer(modifier = Modifier.height(16.dp))
+
             Button(
                 onClick = {navController.navigate(Routes.MenuScreen.route)},
                 colors = ButtonDefaults.buttonColors(
                     containerColor = colorResource(R.color.yellow_custom),
                     contentColor = Color.Black),
-            ) {
+                ) {
                 Text(
                     text = "Return to menu",
                     fontSize = 30.sp)
-            }
+                }
         }
 
     }
